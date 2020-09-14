b66dd903dbcd11eeca0923f8e67b6e778b2016fc
JI-76 <johnison76@hotmail.com>
2020-09-14 16:21:09 -0700

    Javascript 1

--- lightdarkmode.js
+++ lightdarkmode.js
@@ -1,2 +1,19 @@
 // lightdarkmode.js
 
+// Add const to handle HTML Elements:
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+// Switch Theme dynamically
+function switchTheme(event) {
+    //console.log(event.target.checked);
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+
+
+// Event Listener when a chekckbox is changed
+toggleSwitch.addEventListener('change', switchTheme);
